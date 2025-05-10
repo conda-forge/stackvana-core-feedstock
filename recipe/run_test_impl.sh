396ff6909478aa3c129a5a5691ee015f877d2866
beckermr <becker.mr@gmail.com>
2025-05-10 06:55:49 -0500

    fix: need conda

--- recipe/run_test_impl.sh
+++ recipe/run_test_impl.sh
@@ -52,7 +52,7 @@ fi
 setup pex_exceptions
 python -c "import lsst.pex.exceptions"
 
-latest_rubin_env=$(conda search rubin-env-nosysroot --json | jq -r '."rubin-env-nosysroot"[-1].version')
+latest_rubin_env=$(conda search -c conda-forge rubin-env-nosysroot --json | jq -r '."rubin-env-nosysroot"[-1].version')
 curr_rubin_env=$(conda list --json | jq -r '.[] | select(.name == "rubin-env-nosysroot").version')
 
 if [[ "${latest_rubin_env}" != "${curr_rubin_env}" ]]
@@ -62,71 +62,3 @@ then
     echo "latest:           ${latest_rubin_env}"
     exit 1
 fi
-
-# #!/bin/bash
-# if [[ ! ${LSST_HOME} ]]
-# then
-#     echo "LSST_HOME is not set!"
-#     exit 1
-# fi
-
-# if [[ ! ${STACKVANA_ACTIVATED} ]]
-# then
-#     echo "STACKVANA_ACTIVATED is not set!"
-#     exit 1
-# fi
-
-# echo "
-# environment:"
-# env | sort
-
-# echo "
-# eups runs:"
-# {
-#     eups -h
-# } || {
-#     exit 1
-# }
-
-# echo "
-# eups list:"
-# {
-#     eups list
-# } || {
-#     exit 1
-# }
-
-
-# # this should work
-# echo "attempting to build 'pex_exceptions' ..."
-# stackvana-build pex_exceptions
-# echo " "
-
-# echo -n "setting up 'pex_exceptions' ... "
-# setup pex_exceptions
-# if [ -n "${PEX_EXCEPTIONS_DIR}" ]; then
-#     echo "worked!"
-#     # try an import
-#     python -c "import lsst.pex.exceptions"
-# else
-#     echo "failed!"
-#     exit 1
-# fi
-# # val=$(setup -j pex_exceptions 2>&1)
-# # if [[ ! ${val} ]]; then
-# #     echo "worked!"
-# # else
-# #     echo "failed!"
-# #     exit 1
-# # fi
-
-# latest_rubin_env=$(conda search -c conda-forge rubin-env-nosysroot --json | jq -r '."rubin-env-nosysroot"[-1].version')
-# curr_rubin_env=$(micromamba list --json | jq -r '.[] | select(.name == "rubin-env-nosysroot").version')
-
-# if [[ "${latest_rubin_env}" != "${curr_rubin_env}" ]]
-# then
-#     echo "rubin-env is not up to date!"
-#     echo "pinned in recipe: ${curr_rubin_env}"
-#     echo "latest:           ${latest_rubin_env}"
-#     exit 1
-# fi
